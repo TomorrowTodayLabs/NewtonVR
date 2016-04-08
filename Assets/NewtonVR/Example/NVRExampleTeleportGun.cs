d290e4fd566a202c6c18b2fbcf7e727a04cdc838
Nick Abel <nick@tomorrowtodaylabs.com>
2016-04-07 20:36:19 -0700

    Teleport test

---
+++ Assets/NewtonVR/Example/NVRExampleTeleportGun.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace NewtonVR.Example
+{
+    [RequireComponent (typeof(NVRTeleporter))]
+    public class NVRExampleTeleportGun : NVRInteractableItem
+    {
+        public Transform FirePoint;
+        [HideInInspector]
+        public NVRTeleporter Teleporter;
+
+        private NVRPlayer AttachedPlayer;
+
+        private RaycastHit NewLocationInfo;
+
+        protected override void Start()
+        {
+            base.Start();
+            Teleporter = this.GetComponent<NVRTeleporter>();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+        }
+
+        public override void BeginInteraction(NVRHand hand)
+        {
+            base.BeginInteraction(hand);
+            AttachedPlayer = hand.GetComponentInParent<NVRPlayer>();
+        }
+
+        public override void UseButtonDown()
+        {
+            base.UseButtonDown();
+
+            Ray TeleportRay = new Ray(FirePoint.position, FirePoint.forward);
+            Debug.DrawRay(FirePoint.position, FirePoint.forward, Color.red, 4.0f);
+            Physics.Raycast(TeleportRay, out NewLocationInfo);
+            Debug.Log(NewLocationInfo.point.ToString());
+            Teleporter.Teleport(AttachedPlayer, NewLocationInfo.point);
+            
+        }
+    }
+}
--- Assets/NewtonVR/NVRTeleporter.cs
+++ Assets/NewtonVR/NVRTeleporter.cs
@@ -6,11 +6,48 @@ namespace NewtonVR
 {
     public class NVRTeleporter : MonoBehaviour
     {
-        private NVRHand Hand;
+        //private NVRHand Hand;
+
+        [HideInInspector]
+        public bool IsTeleporting;
 
         private void Start()
         {
-            Hand = this.GetComponent<NVRHand>();
+            //Hand = this.GetComponent<NVRHand>();
+            IsTeleporting = false;
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private IEnumerator _Teleport(NVRPlayer player, Vector3 newPositionBase)
+        {
+            IsTeleporting = true;
+            float CurrentHeight;
+
+            while(IsTeleporting)
+            {
+                yield return null;
+                CurrentHeight = player.transform.position.y;
+                player.transform.position = (newPositionBase);
+                IsTeleporting = false;
+            }
+
+        }
+
+        public void Teleport(NVRPlayer player, Vector3 newPositionBase)
+        {
+            if(IsTeleporting == false)
+            {
+                StartCoroutine(_Teleport(player, newPositionBase));
+            }
+            else
+            {
+                // Already teleporting
+            }
         }
     }
 }
+
