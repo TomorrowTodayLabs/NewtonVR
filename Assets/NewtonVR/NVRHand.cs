59a6055bc2c6026dea14b221c1a82eeffd591d56
Dinos Tsiknis <dinos.tsiknis@charmgames.com>
2016-06-09 11:28:15 -0700

    Added NVRGhostRenderer component type for ghost renderers to inherit from to avoid pulling in other renderers attached to the hand inadvertently as ghost renderers

--- Assets/NewtonVR/NVRHand.cs
+++ Assets/NewtonVR/NVRHand.cs
@@ -677,7 +677,7 @@ namespace NewtonVR
                 Color transparentcolor = Color.white;
                 transparentcolor.a = (float)VisibilityLevel.Ghost / 100f;
 
-                GhostRenderers = this.GetComponentsInChildren<Renderer>();
+                GhostRenderers = this.GetComponentsInChildren<NVRGhostRenderer>();
                 for (int rendererIndex = 0; rendererIndex < GhostRenderers.Length; rendererIndex++)
                 {
                     NVRHelpers.SetTransparent(GhostRenderers[rendererIndex].material, transparentcolor);
@@ -691,7 +691,7 @@ namespace NewtonVR
                 Color transparentcolor = Color.white;
                 transparentcolor.a = (float)VisibilityLevel.Ghost / 100f;
 
-                GhostRenderers = this.GetComponentsInChildren<Renderer>();
+                GhostRenderers = this.GetComponentsInChildren<NVRGhostRenderer>();
                 for (int rendererIndex = 0; rendererIndex < GhostRenderers.Length; rendererIndex++)
                 {
                     NVRHelpers.SetTransparent(GhostRenderers[rendererIndex].material, transparentcolor);
