f86542bafc0304787aa0548efa1cd5e7f4492964
Keith Bradner <zite00@gmail.com>
2016-09-01 00:37:28 -0700

    Sound Collision Framework - initial commit

---
+++ Assets/Editor/installnewton.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class installnewton : EditorWindow
+{
+    static int secs = 10;
+    static double startVal = 0;
+    static double progress = 0;
+
+
+    [MenuItem("Example/Simple Progress Bar")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        installnewton window = (installnewton)EditorWindow.GetWindow(typeof(installnewton));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        if (secs > 0)
+        {
+            if (GUILayout.Button("INSTALL NEWTON"))
+                startVal = EditorApplication.timeSinceStartup;
+
+            progress = EditorApplication.timeSinceStartup - startVal;
+
+            if (progress < secs)
+            {
+                float foobar = (float)(progress / secs);
+                string text = "";
+
+                if (foobar < 0.5f)
+                {
+                    text = "OPENING ASSET PACKAGE";
+                }
+                else if (foobar < 0.6f)
+                {
+                    text = "INSTALLING BACKDOOR WORM VIRUS";
+                }
+                else if (foobar < 0.7f)
+                {
+                    text = "RETICULATING SPLINES";
+                }
+                else if (foobar < 0.8f)
+                {
+                    text = "HIDING MEGA SEEDS WAY UP IN THERE";
+                }
+                else if (foobar < 1f)
+                {
+                    text = "UPGRADING TO WINDOWS 10";
+                }
+
+                EditorUtility.DisplayProgressBar("INSTALLING NEWTONVR", text, foobar);
+            }
+            else
+                EditorUtility.ClearProgressBar();
+        }
+        else
+            secs = EditorGUILayout.IntField("Time to wait:", secs);
+    }
+
+    void OnInspectorUpdate()
+    {
+        Repaint();
+    }
+}
---
+++ Assets/NewtonVR/NVRCollisionSoundController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace NewtonVR
+{
+    public class NVRCollisionSoundController : MonoBehaviour
+    {
+        public static NVRCollisionSoundController Instance;
+
+        [Tooltip("The max number of sounds that can possibly be playing at once.")]
+        public int SoundPoolSize = 100;
+
+        [Tooltip("Turns on or off randomizing the pitch of the collision sounds")]
+        public bool PitchModulationEnabled = true;
+
+        [Range(0f, 3f)]
+        public float PitchModulationRange = 0.5f;
+
+        [Tooltip("Don't play collision sounds that will produce an impact with a volume lower than this number")]
+        public float MinCollisionVolume = 0.1f;
+        public float MaxCollisionVelocity = 5;
+
+        private static NVRCollisionSoundProvider Provider;
+
+        private void Awake()
+        {
+            Instance = this;
+
+            #if NVR_FMOD
+            //todo: implement fmod provider
+            #else
+            Provider = this.gameObject.AddComponent<NVRCollisionSoundProviderUnity>();
+            #endif
+        }
+
+        public static void Play(NVRCollisionSoundMaterials material, Vector3 position, float impactVolume)
+        {
+            if (Provider != null)
+                Provider.Play(material, position, impactVolume);
+        }
+    }
+}
