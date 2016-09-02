9a4698774b70b0fa592c5aba0210cd9687236947
Nicholas Abel <nick.a.abel@gmail.com>
2016-09-02 11:17:05 -0700

    Commit of dummy NVRCollisionSoundProvider to allow for compilation, until next commit of correct NVRSoundCollsionProvider

---
+++ Assets/NewtonVR/NVRCollisionSoundProvider.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+ * THIS IS A DUMMY FILE, COMMITTED 9-2-16 BY NICHOLAS ABEL, NICK@TOMORROWDAYLABS.COM
+ * Committed in place of the actual NVRCollisionSoundProvider to allow for compilation.
+ * **/
+
+namespace NewtonVR
+{
+    public class NVRCollisionSoundProvider : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public virtual void Awake()
+        {
+
+        }
+
+        public virtual void Play(NVRCollisionSoundMaterials material, Vector3 position, float impactVolume)
+        {
+
+        }
+    }
+}
