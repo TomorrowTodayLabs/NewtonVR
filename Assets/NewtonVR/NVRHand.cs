e0a6f7b59fb38d6dc3cafe6bd104da1841369c16
Nick Abel <nick@tomorrowtodaylabs.com>
2016-09-08 18:34:48 -0700

    NVRHand now initializes correctly based on if a Physical Controller is present. Added functions for accessing Controller Device velocity and angular velocity.

--- Assets/NewtonVR/NVRHand.cs
+++ Assets/NewtonVR/NVRHand.cs
@@ -82,6 +82,7 @@ namespace NewtonVR
 
         protected virtual void Awake()
         {
+            
             CurrentlyHoveringOver = new Dictionary<NVRInteractable, Dictionary<Collider, float>>();
 
             LastPositions = new Vector3[EstimationSamples];
@@ -499,6 +500,8 @@ namespace NewtonVR
 
         protected virtual void OnEnable()
         {
+            VisibilityLocked = false;
+
             if (CustomModel != null)
             {
                 this.GetComponentInChildren<SteamVR_RenderModel>().enabled = false;
@@ -687,13 +690,28 @@ namespace NewtonVR
 
             if (NVRPlayer.Instance.PhysicalHands == true)
             {
+                bool InitialState = false;
+
                 if (PhysicalController != null)
                 {
+                    if (PhysicalController.State == true)
+                    {
+                        InitialState = true;
+                    }
+                    else
+                    {
+                        InitialState = false;
+                    }
                     PhysicalController.Kill();
                 }
 
                 PhysicalController = this.gameObject.AddComponent<NVRPhysicalController>();
-                PhysicalController.Initialize(this, false);
+                PhysicalController.Initialize(this, InitialState);
+
+                if (InitialState == true)
+                {
+                    ForceGhost();
+                }
 
                 Color transparentcolor = Color.white;
                 transparentcolor.a = (float)VisibilityLevel.Ghost / 100f;
@@ -755,6 +773,22 @@ namespace NewtonVR
         {
             SteamVR_Utils.Event.Remove("render_model_loaded", RenderModelLoaded);
             SteamVR_Utils.Event.Remove("new_poses_applied", OnNewPosesApplied);
+        }
+
+        public void GetDeviceVelocity(out Vector3 velocity, out Vector3 angularVelocity)
+        {
+            velocity = Controller.velocity;
+            angularVelocity = Controller.angularVelocity;
+        }
+
+        public Vector3 GetDeviceVelocity()
+        {
+            return Controller.velocity;
+        }
+
+        public Vector3 GetDeviceAngularVelocity()
+        {
+            return Controller.angularVelocity;
         }
     }
     
