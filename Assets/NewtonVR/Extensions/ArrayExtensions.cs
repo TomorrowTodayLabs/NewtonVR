e5b4c1f4abf728f39286e74a554872ff355a4ff9
makai <makaimariano@gmail.com>
2016-09-17 18:10:31 -0400

    Array extensions

---
+++ Assets/NewtonVR/Extensions/ArrayExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace NewtonVR
+{
+    using System;
+    using System.Collections.Generic;;
+    static class ArrayExtensions
+    {
+        public static IList<TB> Map<TA, TB>(this IList<TA> collection, Func<TA, TB> convert)
+        {
+            var converted = new TB[collection.Count];
+
+            for (var i = 0; i < converted.Length; i++)
+                converted[i] = convert(collection[i]);
+
+            return converted;
+        }
+    }
+}
