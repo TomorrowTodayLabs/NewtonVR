8cae9b86d909636c3813ada0e309f0351db106a1
makai <makaimariano@gmail.com>
2016-09-17 17:51:03 -0400

    Reduce - less garbage than LINQ aggregate

--- Assets/NewtonVR/Extensions/IListExtensions.cs
+++ Assets/NewtonVR/Extensions/IListExtensions.cs
@@ -9,5 +9,14 @@
             for (var i = 0; i < collection.Count; i++)
                 action(collection[i]);
         }
+
+        public static T Reduce<T>(this IList<T> collection, Func<T, T, T> monoid, T start)
+        {
+            var reduction = start;
+
+            collection.Iterate(element => monoid(reduction, element));
+
+            return reduction;
+        }
     }
 }
