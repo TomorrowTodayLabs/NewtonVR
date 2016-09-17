69a2e373400ee379775e35c674258e8e528327e4
makai <makaimariano@gmail.com>
2016-09-17 18:12:26 -0400

    Fix implementation

--- Assets/NewtonVR/Extensions/ArrayExtensions.cs
+++ Assets/NewtonVR/Extensions/ArrayExtensions.cs
@@ -1,12 +1,11 @@
 ﻿namespace NewtonVR
 {
     using System;
-    using System.Collections.Generic;;
     static class ArrayExtensions
     {
-        public static IList<TB> Map<TA, TB>(this IList<TA> collection, Func<TA, TB> convert)
+        public static TB[] Map<TA, TB>(this TA[] collection, Func<TA, TB> convert)
         {
-            var converted = new TB[collection.Count];
+            var converted = new TB[collection.Length];
 
             for (var i = 0; i < converted.Length; i++)
                 converted[i] = convert(collection[i]);
