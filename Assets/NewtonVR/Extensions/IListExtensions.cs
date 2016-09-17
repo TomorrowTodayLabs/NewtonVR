b17f64409c1d16495d29486df809ce7e07660cf0
makai <makaimariano@gmail.com>
2016-09-17 17:58:11 -0400

    Map

--- Assets/NewtonVR/Extensions/IListExtensions.cs
+++ Assets/NewtonVR/Extensions/IListExtensions.cs
@@ -10,6 +10,16 @@
                 action(collection[i]);
         }
 
+        public static IList<TB> Map<TA, TB>(this IList<TA> collection, Func<TA, TB> convert)
+        {
+            var converted = new TB[collection.Count];
+
+            for (var i = 0; i < converted.Length; i++)
+                converted[i] = convert(collection[i]);
+
+            return converted;
+        }
+
         public static T Reduce<T>(this IList<T> collection, Func<T, T, T> accumulator, T start)
         {
             var reduction = start;
