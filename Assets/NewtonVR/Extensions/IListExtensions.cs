228017686d4f40defed7bd1aa16eee7e578817fa
makai <makaimariano@gmail.com>
2016-09-17 17:37:11 -0400

    Iterate extension - same performance as for loop

---
+++ Assets/NewtonVR/Extensions/IListExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace NewtonVR
+{
+    using System;
+    using System.Collections.Generic;
+    static class IListExtensions
+    {
+        public static void Iterate<T>(this IList<T> collection, Action<T> action)
+        {
+            for (var i = 0; i < collection.Count; i++)
+                action(collection[i]);
+        }
+    }
+}
