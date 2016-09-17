8d4657f2bc2d90acc2538de9173c5cc4b5bab3fb
makai <makaimariano@gmail.com>
2016-09-17 18:53:40 -0400

    Remove redundancy

--- Assets/NewtonVR/Example/NVRExampleGun.cs
+++ Assets/NewtonVR/Example/NVRExampleGun.cs
@@ -14,13 +14,13 @@ namespace NewtonVR.Example
         {
             base.UseButtonDown();
 
-            GameObject bullet = GameObject.Instantiate(BulletPrefab);
+            GameObject bullet = Instantiate(BulletPrefab);
             bullet.transform.position = FirePoint.position;
             bullet.transform.forward = FirePoint.forward;
 
             bullet.GetComponent<Rigidbody>().AddRelativeForce(BulletForce);
 
-            AttachedHand.TriggerHapticPulse(500, Valve.VR.EVRButtonId.k_EButton_Axis0);
+            AttachedHand.TriggerHapticPulse();
         }
     }
 }
