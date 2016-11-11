e8d446f36556f47f113d09f3991d2d000caaafbf
Keith Bradner <zite00@gmail.com>
2016-11-10 20:07:44 -0800

    Updating version and unitypackage

--- Assets/NewtonVR/NVRPlayer.cs
+++ Assets/NewtonVR/NVRPlayer.cs
@@ -8,7 +8,7 @@ namespace NewtonVR
 {
     public class NVRPlayer : MonoBehaviour
     {
-        public const decimal NewtonVRVersion = 0.92m;
+        public const decimal NewtonVRVersion = 0.93m;
 
         public static List<NVRPlayer> Instances = new List<NVRPlayer>();
         public static NVRPlayer Instance
