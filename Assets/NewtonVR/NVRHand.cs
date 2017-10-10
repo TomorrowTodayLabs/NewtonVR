35bad7e5be6912c38a5c3db2a0fac5a23b0a41c0
Ryan <ryan@campfireunion.com>
2017-10-10 15:56:02 -0500

    Won't pick up disabled objects.

--- Assets/NewtonVR/NVRHand.cs
+++ Assets/NewtonVR/NVRHand.cs
@@ -603,6 +603,9 @@ namespace NewtonVR
                 {
                     if (hovering.Key == null)
                         continue;
+					
+					if (!hovering.Key.gameObject.activeInHierarchy)
+						continue;
 
                     float distance = Vector3.Distance(collider.transform.position, hovering.Key.transform.position);
                     if (distance < closestDistance)
