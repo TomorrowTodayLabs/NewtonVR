6f744437415582bf052ea6753a12caaf38b41b96
Paulo Salvatore <salvatoregames@gmail.com>
2018-02-11 19:20:40 -0200

    Improvements on NVRButton system

--- Assets/NewtonVR/NVRButton.cs
+++ Assets/NewtonVR/NVRButton.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 namespace NewtonVR
 {
@@ -7,8 +6,17 @@ namespace NewtonVR
     {
         public Rigidbody Rigidbody;
 
+        [Tooltip("The direction of button movement")]
+        public ButtonDirections ButtonDirection = ButtonDirections.y;
+
+        [Tooltip("The limit direction of button movement")]
+        public ButtonLimitDirections ButtonLimitDirection = ButtonLimitDirections.None;
+
         [Tooltip("The (worldspace) distance from the initial position you have to push the button for it to register as pushed")]
         public float DistanceToEngage = 0.075f;
+
+        [Tooltip("Maximum button distance from start position (based on axis selected on ButtonDirection variable)")]
+        public float MaxDistance;
 
         [Tooltip("Is set to true when the button has been pressed down this update frame")]
         public bool ButtonDown = false;
@@ -28,6 +36,7 @@ namespace NewtonVR
         protected float PositionMagic = 1000f;
 
         protected float CurrentDistance = -1;
+        private float RealMaxDistance;
 
         private Vector3 InitialLocalPosition;
         private Vector3 ConstrainedPosition;
@@ -37,23 +46,23 @@ namespace NewtonVR
 
         private void Awake()
         {
-            InitialPosition = new GameObject(string.Format("[{0}] Initial Position", this.gameObject.name)).transform;
-            InitialPosition.parent = this.transform.parent;
-            InitialPosition.localPosition = Vector3.zero;
-            InitialPosition.localRotation = Quaternion.identity;
+            InitialPosition = new GameObject(string.Format("[{0}] Initial Position", gameObject.name)).transform;
+            InitialPosition.parent = transform.parent;
+            InitialPosition.localPosition = transform.localPosition;
+            InitialPosition.localRotation = transform.localRotation;
 
             if (Rigidbody == null)
-                Rigidbody = this.GetComponent<Rigidbody>();
+                Rigidbody = GetComponent<Rigidbody>();
 
             if (Rigidbody == null)
             {
                 Debug.LogError("There is no rigidbody attached to this button.");
             }
 
-            InitialLocalPosition = this.transform.localPosition;
+            InitialLocalPosition = transform.localPosition;
             ConstrainedPosition = InitialLocalPosition;
 
-            InitialLocalRotation = this.transform.localRotation;
+            InitialLocalRotation = transform.localRotation;
             ConstrainedRotation = InitialLocalRotation;
         }
 
@@ -61,10 +70,10 @@ namespace NewtonVR
         {
             ConstrainPosition();
 
-            CurrentDistance = Vector3.Distance(this.transform.position, InitialPosition.position);
+            CurrentDistance = Vector3.Distance(transform.position, InitialPosition.position);
 
-            Vector3 PositionDelta = InitialPosition.position - this.transform.position;
-            this.Rigidbody.velocity = PositionDelta * PositionMagic * Time.deltaTime;
+            Vector3 PositionDelta = InitialPosition.position - transform.position;
+            Rigidbody.velocity = PositionDelta * PositionMagic * Time.deltaTime;
         }
 
         private void Update()
@@ -85,14 +94,160 @@ namespace NewtonVR
 
         private void ConstrainPosition()
         {
-            ConstrainedPosition.y = this.transform.localPosition.y;
-            this.transform.localPosition = ConstrainedPosition;
-            this.transform.localRotation = ConstrainedRotation;
+            // Calculates Real Max Distance
+
+            RealMaxDistance =
+                MaxDistance == 0
+                    ? Mathf.Infinity
+                    : Mathf.Abs(MaxDistance);
+
+            if (ButtonLimitDirection == ButtonLimitDirections.Negative)
+                RealMaxDistance *= -1f;
+
+            switch (ButtonDirection)
+            {
+                case ButtonDirections.x:
+                    RealMaxDistance = InitialLocalPosition.x + RealMaxDistance;
+                    break;
+
+                case ButtonDirections.y:
+                    RealMaxDistance = InitialLocalPosition.y + RealMaxDistance;
+                    break;
+
+                case ButtonDirections.z:
+                    RealMaxDistance = InitialLocalPosition.z + RealMaxDistance;
+                    break;
+
+                default:
+                    break;
+            }
+
+            // Check which direction the button will move to and update the constrained position
+            switch (ButtonDirection)
+            {
+                case ButtonDirections.x:
+                    if (ValidateButtonMovement(transform.localPosition.x, InitialLocalPosition.x))
+                    {
+                        if (ButtonLimitDirection == ButtonLimitDirections.None)
+                        {
+                            ConstrainedPosition.x =
+                                Mathf.Clamp(
+                                    transform.localPosition.x,
+                                    -RealMaxDistance,
+                                    RealMaxDistance
+                                );
+                        }
+                        else
+                        {
+                            ConstrainedPosition.x =
+                                Mathf.Clamp(
+                                    transform.localPosition.x,
+                                    Mathf.Min(InitialLocalPosition.x, RealMaxDistance),
+                                    Mathf.Max(InitialLocalPosition.x, RealMaxDistance)
+                                );
+                        }
+                    }
+
+                    break;
+
+                case ButtonDirections.y:
+                    if (ValidateButtonMovement(transform.localPosition.y, InitialLocalPosition.y))
+                    {
+                        if (ButtonLimitDirection == ButtonLimitDirections.None)
+                        {
+                            ConstrainedPosition.y =
+                                Mathf.Clamp(
+                                    transform.localPosition.y,
+                                    -RealMaxDistance,
+                                    RealMaxDistance
+                                );
+                        }
+                        else
+                        {
+                            ConstrainedPosition.y =
+                            Mathf.Clamp(
+                                transform.localPosition.y,
+                                Mathf.Min(InitialLocalPosition.y, RealMaxDistance),
+                                Mathf.Max(InitialLocalPosition.y, RealMaxDistance)
+                            );
+                        }
+                    }
+
+                    break;
+
+                case ButtonDirections.z:
+                    if (ValidateButtonMovement(transform.localPosition.z, InitialLocalPosition.z))
+                    {
+                        if (ButtonLimitDirection == ButtonLimitDirections.None)
+                        {
+                            ConstrainedPosition.z =
+                                Mathf.Clamp(
+                                    transform.localPosition.z,
+                                    -RealMaxDistance,
+                                    RealMaxDistance
+                                );
+                        }
+                        else
+                        {
+                            ConstrainedPosition.z =
+                            Mathf.Clamp(
+                                transform.localPosition.z,
+                                Mathf.Min(InitialLocalPosition.z, RealMaxDistance),
+                                Mathf.Max(InitialLocalPosition.z, RealMaxDistance)
+                            );
+                        }
+                    }
+
+                    break;
+
+                default:
+                    break;
+            }
+
+            transform.localPosition = ConstrainedPosition;
+            transform.localRotation = ConstrainedRotation;
+        }
+
+        // Validates if button can move on positive or negative axis
+        private bool ValidateButtonMovement(float currentValue, float initialValue)
+        {
+            float difference = currentValue - initialValue;
+
+            switch (ButtonLimitDirection)
+            {
+                case ButtonLimitDirections.None:
+                    break;
+
+                case ButtonLimitDirections.Positive:
+                    return difference > 0;
+
+                case ButtonLimitDirections.Negative:
+                    return difference < 0;
+
+                default:
+                    break;
+            }
+
+            return true;
         }
 
         private void LateUpdate()
         {
             ConstrainPosition();
+        }
+
+        public enum ButtonDirections
+        {
+            x,
+            y,
+            z
+        }
+
+        public enum ButtonLimitDirections
+        {
+            None,
+            Positive,
+            Negative
         }
     }
 }
